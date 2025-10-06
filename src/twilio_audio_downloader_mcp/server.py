209f672eb9cb9ad0064052c2f94503854baef78f
Eugene Karankow <Eugene_Karankow@epam.com>
2025-10-06 16:47:48 +0200

    return blob

--- src/twilio_audio_downloader_mcp/server.py
+++ src/twilio_audio_downloader_mcp/server.py
@@ -234,7 +234,7 @@ def download_twilio_audio(url: str) -> Dict[str, Any]:
         # ).dict()
 
         blob = BlobResourceContents(
-            uri="twilio://{0}".format(filename),
+            uri="file://{0}".format(filename),
             data=audio_data,
             mime_type=content_type
         )
